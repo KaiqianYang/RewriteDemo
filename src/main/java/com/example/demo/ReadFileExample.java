f2dd051e0a4e32e8205278a1493b67b29db0ffce
kaiqianyang <kaiqianyang@microsoftcom>
2023-02-17 11:17:09 +0800

    update

--- src/main/java/com/example/demo/ReadFileExample.java
+++ src/main/java/com/example/demo/ReadFileExample.java
@@ -3,42 +3,10 @@ package com.example.demo;
 import java.io.*;
 
 public class ReadFileExample {
-    public static void LinuxFile() {
-            BufferedReader br = new BufferedReader(new FileReader("/home/project/targetfile"));
-            br = new BufferedReader(new FileReader("../targetfile"));
-    }
 
-    public static void windowsBackslashes() {
+    public static void eg() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("C:\\Windows\\example.txt"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void windowsforwardSlashes(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("C:/Windows/example.txt"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //comments
-    public static void windowsFileUrl(String[] args) {
-        try {
-            //BufferedReader br = new BufferedReader(new FileReader("file://localhost/c:/WINDOWS/clock.avi"));
-            BufferedReader br = new BufferedReader(new FileReader("file:/c:/WINDOWS/clock.avi"));
+            BufferedReader br = new BufferedReader(new FileReader("../pom.xml"));
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
